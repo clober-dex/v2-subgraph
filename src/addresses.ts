5201fc77860ebc6b62150da5e7303312a9436f9a
Xavier <ladig1017@gmail.com>
2024-06-28 19:36:10 +0900

    feat: apply fallback multicall3 address

--- src/addresses.ts
+++ src/addresses.ts
@@ -3,20 +3,31 @@ import { Address, BigInt } from '@graphprotocol/graph-ts'
 import { Multicall3 } from '../generated/BookManager/Multicall3'
 
 const MULTICALL3_ADDRESS = '0xcA11bde05977b3631167028862bE2a173976CA11'
+const MULTICALL3_FALLBACK_ADDRESS = '0xF9cda624FBC7e059355ce98a31693d299FACd963'
 const ARBITRUM_SEPOLIA = BigInt.fromI32(421614)
 const BASE = BigInt.fromI32(8453)
 const BERA_TESTNET = BigInt.fromI32(80085)
+const ZKSYNC_ERA = BigInt.fromI32(324)
+const ZKSYNC_ERA_SEPOLIA = BigInt.fromI32(300)
 
 export function getChainId(): BigInt {
   const multiCall = Multicall3.bind(Address.fromString(MULTICALL3_ADDRESS))
-  return multiCall.getChainId()
+  const chainId = multiCall.try_getChainId()
+  if (chainId.reverted) {
+    const multiCallFallback = Multicall3.bind(
+      Address.fromString(MULTICALL3_FALLBACK_ADDRESS),
+    )
+    const chainIdFallback = multiCallFallback.try_getChainId()
+    if (chainIdFallback.reverted) {
+      return BigInt.fromI32(0)
+    } else {
+      return chainIdFallback.value
+    }
+  }
+  return chainId.value
 }
 
 export function getControllerAddress(): string {
-  // for zksync sepolia test, just return directly
-  // return '0xA253A7c6C26E0a6E7eAbaAbCD8b1cD43A2468c48'
-  // for zksync, just return directly
-  // return '0x11BB911a7F0EE10d4cEB569f3D7d6b741dEbdfcc'
   const chainId = getChainId()
   if (chainId == ARBITRUM_SEPOLIA) {
     return '0x91101543D3Bd3e919dAd034Bf978ef9d87290993'
@@ -24,6 +35,10 @@ export function getControllerAddress(): string {
     return '0xA694fDd88E7FEE1f5EBF878153B68ADb2ce6EbbF'
   } else if (chainId == BERA_TESTNET) {
     return '0x1Aa68597c14F3f950E2683fA7a975fc9CdAcC484'
+  } else if (chainId == ZKSYNC_ERA) {
+    return '0x11BB911a7F0EE10d4cEB569f3D7d6b741dEbdfcc'
+  } else if (chainId == ZKSYNC_ERA_SEPOLIA) {
+    return '0xA253A7c6C26E0a6E7eAbaAbCD8b1cD43A2468c48'
   } else {
     return '0x91101543D3Bd3e919dAd034Bf978ef9d87290993'
   }
