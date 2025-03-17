37dc06d988ccd72d610f9251b7a6b4571241d95b
graykode <nlkey2022@gmail.com>
2025-03-17 21:42:35 +0900

    fix: contract address

--- src/addresses.ts
+++ src/addresses.ts
@@ -11,6 +11,7 @@ export const ZKSYNC_ERA = BigInt.fromI32(324)
 export const ZKSYNC_ERA_SEPOLIA = BigInt.fromI32(300)
 export const MITOSIS_TESTNET = BigInt.fromI32(124832)
 export const SONIC_MAINNET = BigInt.fromI32(146)
+export const MONAD_TESTNET = BigInt.fromI32(10143)
 
 export function getChainId(): BigInt {
   const multiCall = Multicall3.bind(Address.fromString(MULTICALL3_ADDRESS))
@@ -45,8 +46,10 @@ export function getControllerAddress(): string {
     return '0xA253A7c6C26E0a6E7eAbaAbCD8b1cD43A2468c48'
   } else if (chainId == SONIC_MAINNET) {
     return '0xADc0CC0c3Ea12e57b8BcB7d7C8ac03222487E337'
-  } else {
+  } else if (chainId == MONAD_TESTNET) {
     return '0xE64aCE1bF550E57461cd4e24706633d7faC9D7b0'
+  } else {
+    throw new Error('Chain ID not supported')
   }
 }
 
@@ -66,8 +69,10 @@ export function getRebalancerAddress(): string {
     return '0xA253A7c6C26E0a6E7eAbaAbCD8b1cD43A2468c48'
   } else if (chainId == SONIC_MAINNET) {
     return '0x46107Ec44112675689053b96aea2127fD952bd47'
+  } else if (chainId == MONAD_TESTNET) {
+    return '0x6d8fa3025b6d6604309Ca257563CcA358c0CF1AA'
   } else {
-    return '0x30b4e9215322B5d0c290249126bCf96C2Ca8e948'
+    throw new Error('Chain ID not supported')
   }
 }
 
@@ -87,7 +92,9 @@ export function getSimpleOracleStrategyAddress(): string {
     return '0xA253A7c6C26E0a6E7eAbaAbCD8b1cD43A2468c48'
   } else if (chainId == SONIC_MAINNET) {
     return '0xdd30f831bEB51fBF33E3D579e5529d3B1495554f'
+  } else if (chainId == MONAD_TESTNET) {
+    return '0x9eE708876804F9416B3C1a1aad0c016dee9DD804'
   } else {
-    return '0x540488b54c8DE6e44Db7553c3A2C4ABEb09Fc69C'
+    throw new Error('Chain ID not supported')
   }
 }
