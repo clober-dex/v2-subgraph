c65f7e0464dbde6fffd31cafc527ca6cafea5f58
JChoy <jchoyeclipse@gmail.com>
2025-04-15 14:55:54 +0900

    refactor: make use helper functions

--- src/router-gateway.ts
+++ src/router-gateway.ts
@@ -1,40 +1,20 @@
-import { BigInt } from '@graphprotocol/graph-ts'
-
 import { Swap } from '../generated/RouterGateway/RouterGateway'
-import { TransactionSnapshot, WalletSnapshot } from '../generated/schema'
 
-import { getOrCreateSnapshot, getOrCreateVolumeSnapshot } from './helpers'
+import {
+  getOrCreateSnapshot,
+  getOrCreateVolumeSnapshot,
+  updateTransactionsInSnapshot,
+  updateWalletsInSnapshot,
+} from './helpers'
 
 export function handleSwap(event: Swap): void {
   const snapshot = getOrCreateSnapshot(event.block.timestamp)
 
   // Create TransactionSnapshot
-  const transactionSnapshotId = snapshot.id
-    .concat('-')
-    .concat(event.transaction.hash.toHexString())
-  let transactionSnapshot = TransactionSnapshot.load(transactionSnapshotId)
-  if (transactionSnapshot === null) {
-    transactionSnapshot = new TransactionSnapshot(transactionSnapshotId)
-    transactionSnapshot.txHash = event.transaction.hash.toHexString()
-    transactionSnapshot.timestamp = BigInt.fromString(snapshot.id)
-    transactionSnapshot.save()
-    snapshot.transactionCount = snapshot.transactionCount.plus(
-      BigInt.fromI32(1),
-    )
-  }
+  updateTransactionsInSnapshot(snapshot, event.transaction.hash)
 
   // Create WalletSnapshot
-  const walletSnapshotId = snapshot.id
-    .concat('-')
-    .concat(event.params.user.toHexString())
-  let walletSnapshot = WalletSnapshot.load(walletSnapshotId)
-  if (walletSnapshot === null) {
-    walletSnapshot = new WalletSnapshot(walletSnapshotId)
-    walletSnapshot.wallet = event.params.user.toHexString()
-    walletSnapshot.timestamp = BigInt.fromString(snapshot.id)
-    walletSnapshot.save()
-    snapshot.walletCount = snapshot.walletCount.plus(BigInt.fromI32(1))
-  }
+  updateWalletsInSnapshot(snapshot, event.params.user)
 
   // Create VolumeSnapshot
   const volumeSnapshot = getOrCreateVolumeSnapshot(
