91f4d2878fc41a9858eab85f29ca2e8c71832d37
JChoy <jchoyeclipse@gmail.com>
2025-04-18 18:30:57 +0900

    feat: move fetch functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,5 @@
 import { Address, BigDecimal, BigInt, Bytes } from '@graphprotocol/graph-ts'
 
-import { ERC20 } from '../generated/BookManager/ERC20'
-import { ERC20SymbolBytes } from '../generated/BookManager/ERC20SymbolBytes'
-import { ERC20NameBytes } from '../generated/BookManager/ERC20NameBytes'
 import {
   Book,
   BookTransactionSnapshot,
@@ -21,12 +18,15 @@ import {
   CHART_LOG_INTERVALS,
   encodePoolSpreadProfitId,
   getChainId,
-  getNativeTokenName,
-  getNativeTokenSymbol,
   getUSDCAddress,
   getWETHAddress,
+  normalizeDailyTimestamp,
 } from './utils'
-import { normalizeDailyTimestamp } from './utils/math'
+import {
+  fetchTokenSymbol,
+  fetchTokenName,
+  fetchTokenDecimals,
+} from './blockchain'
 
 export function getOrCreateSnapshot(timestamp: BigInt): Snapshot {
   const dailyNormalizedTimestamp = normalizeDailyTimestamp(timestamp)
@@ -197,73 +197,6 @@ export function createToken(tokenAddress: Address): Token {
   return token
 }
 
-export function fetchTokenSymbol(
-  tokenAddress: Address,
-  chainId: BigInt,
-): string {
-  if (tokenAddress.equals(ADDRESS_ZERO)) {
-    return getNativeTokenSymbol(chainId)
-  }
-  const contract = ERC20.bind(tokenAddress)
-  const contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress)
-
-  // try types string and bytes32 for symbol
-  let symbolValue = 'unknown'
-  const symbolResult = contract.try_symbol()
-  if (symbolResult.reverted) {
-    const symbolResultBytes = contractSymbolBytes.try_symbol()
-    if (!symbolResultBytes.reverted) {
-      // for broken pairs that have no symbol function exposed
-      if (!isNullValue(symbolResultBytes.value.toHexString())) {
-        symbolValue = symbolResultBytes.value.toString()
-      }
-    }
-  } else {
-    symbolValue = symbolResult.value
-  }
-
-  return symbolValue
-}
-
-export function fetchTokenName(tokenAddress: Address, chainId: BigInt): string {
-  if (tokenAddress.equals(ADDRESS_ZERO)) {
-    return getNativeTokenName(chainId)
-  }
-  const contract = ERC20.bind(tokenAddress)
-  const contractNameBytes = ERC20NameBytes.bind(tokenAddress)
-
-  // try types string and bytes32 for name
-  let nameValue = 'unknown'
-  const nameResult = contract.try_name()
-  if (nameResult.reverted) {
-    const nameResultBytes = contractNameBytes.try_name()
-    if (!nameResultBytes.reverted) {
-      // for broken exchanges that have no name function exposed
-      if (!isNullValue(nameResultBytes.value.toHexString())) {
-        nameValue = nameResultBytes.value.toString()
-      }
-    }
-  } else {
-    nameValue = nameResult.value
-  }
-
-  return nameValue
-}
-
-export function fetchTokenDecimals(tokenAddress: Address): BigInt {
-  if (tokenAddress.equals(ADDRESS_ZERO)) {
-    return BigInt.fromI32(18)
-  }
-  const contract = ERC20.bind(tokenAddress)
-  // try types uint8 for decimals
-  let decimalValue = 18
-  const decimalResult = contract.try_decimals()
-  if (!decimalResult.reverted) {
-    decimalValue = decimalResult.value
-  }
-  return BigInt.fromI32(decimalValue as i32)
-}
-
 export function getLatestPoolSpread(): LatestPoolSpread {
   const id = 'latest'
   let latestPoolSpread = LatestPoolSpread.load(id)
@@ -298,11 +231,4 @@ export function getPoolSpreadProfit(timestamp: BigInt): PoolSpreadProfit {
     poolSpreadProfit.accumulatedProfitInUsd = BigDecimal.zero()
   }
   return poolSpreadProfit as PoolSpreadProfit
-}
-
-function isNullValue(value: string): boolean {
-  return (
-    value ==
-    '0x0000000000000000000000000000000000000000000000000000000000000001'
-  )
 }
