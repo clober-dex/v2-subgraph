5b1c228811aca26f9d41456ecc4519b1e29e5914
graykode <nlkey2022@gmail.com>
2025-05-08 20:32:13 +0900

    test: update encodeOrderId tests with new assertions and remove unused tick variable

--- tests/encode.test.ts
+++ tests/encode.test.ts
@@ -1,12 +1,11 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts'
 import { describe, test, assert } from 'matchstick-as'
 
-import { encodeOrderId } from '../src/utils'
+import { encodeOrderId } from '../src/common/order'
 
 class MintFixture {
   bookId: BigInt
   user: Address
-  tick: i32
   orderIndex: BigInt
   uint: BigInt
   provider: Address
@@ -17,7 +16,6 @@ const MINT_FIXTURE: MintFixture = {
     '2903842787083910905150096686205997338709207897290567260368',
   ),
   user: Address.fromString('0x6d8fa3025b6d6604309ca257563cca358c0cf1aa'),
-  tick: i32(-259218),
   orderIndex: BigInt.fromString('267'),
   uint: BigInt.fromString('68227772598'),
   provider: Address.fromString('0x0000000000000000000000000000000000000000'),
@@ -25,16 +23,36 @@ const MINT_FIXTURE: MintFixture = {
 
 describe('encode', () => {
   test('success - encodeOrderId', () => {
-    const orderID = encodeOrderId(
-      MINT_FIXTURE.bookId.toString(),
-      BigInt.fromI32(MINT_FIXTURE.tick as i32),
-      MINT_FIXTURE.orderIndex,
+    assert.assertTrue(
+      encodeOrderId(
+        MINT_FIXTURE.bookId.toString(),
+        BigInt.fromI32(-259218 as i32),
+        MINT_FIXTURE.orderIndex,
+      ) ==
+        BigInt.fromString(
+          '53566444723624360785422944131996696091141564570442382186621897550883387867403',
+        ),
     )
 
     assert.assertTrue(
-      orderID ==
+      encodeOrderId(
+        MINT_FIXTURE.bookId.toString(),
+        BigInt.fromI32(-1 as i32),
+        MINT_FIXTURE.orderIndex,
+      ) ==
         BigInt.fromString(
-          '53566444723624360785422944131996696091141564570442382186621897550883387867403',
+          '53566444723624360785422944131996696091141564570442382186622182562989005078795',
+        ),
+    )
+
+    assert.assertTrue(
+      encodeOrderId(
+        MINT_FIXTURE.bookId.toString(),
+        BigInt.fromI32(-2 as i32),
+        MINT_FIXTURE.orderIndex,
+      ) ==
+        BigInt.fromString(
+          '53566444723624360785422944131996696091141564570442382186622182561889493451019',
         ),
     )
   })
