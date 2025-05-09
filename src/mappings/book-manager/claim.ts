4e05ef29f5db787be21186ab5d293256e22db58d
graykode <nlkey2022@gmail.com>
2025-05-09 14:49:46 +0900

    refactor: add handleClaim export to core.ts

---
+++ src/mappings/book-manager/claim.ts
@@ -0,0 +1,83 @@
+import { store } from '@graphprotocol/graph-ts'
+
+import { Claim } from '../../../generated/BookManager/BookManager'
+import {
+  decodeBookIdFromOrderId,
+  getPendingUnitAmount,
+} from '../../common/order'
+import {
+  getBookOrLog,
+  getOpenOrderOrLog,
+  getTokenOrLog,
+} from '../../common/entity-getters'
+import { unitToBase, unitToQuote } from '../../common/amount'
+import { tickToPrice } from '../../common/tick'
+import { convertTokenToDecimal } from '../../common/utils'
+import { calculateValueUSD, getTokenUSDPrice } from '../../common/pricing'
+
+export function handleClaim(event: Claim): void {
+  if (event.params.unit.isZero()) {
+    return
+  }
+
+  const bookID = decodeBookIdFromOrderId(event.params.orderId)
+  const book = getBookOrLog(bookID, 'CLAIM')
+  if (book === null) {
+    return
+  }
+
+  const openOrderID = event.params.orderId.toString()
+  const openOrder = getOpenOrderOrLog(openOrderID, 'CLAIM')
+  if (openOrder === null) {
+    return
+  }
+
+  const quote = getTokenOrLog(book.quote, 'CLAIM')
+  const base = getTokenOrLog(book.base, 'CLAIM')
+  if (quote && base) {
+    const priceRaw = tickToPrice(openOrder.tick.toI32())
+
+    const quoteAmount = unitToQuote(book.unitSize, event.params.unit)
+    const quoteAmountDecimal = convertTokenToDecimal(
+      quoteAmount,
+      quote.decimals,
+    )
+    const quoteInUSD = getTokenUSDPrice(quote)
+
+    const baseAmount = unitToBase(book.unitSize, event.params.unit, priceRaw)
+    const baseAmountDecimal = convertTokenToDecimal(baseAmount, base.decimals)
+    const baseInUSD = getTokenUSDPrice(base)
+
+    const amountUSD = calculateValueUSD(
+      quoteAmountDecimal,
+      quoteInUSD,
+      baseAmountDecimal,
+      baseInUSD,
+    )
+
+    // claimed data
+    openOrder.claimedUnitAmount = openOrder.claimedUnitAmount.plus(
+      event.params.unit,
+    )
+    openOrder.claimedBaseAmount = openOrder.claimedBaseAmount.plus(baseAmount)
+    openOrder.claimedQuoteAmount =
+      openOrder.claimedQuoteAmount.plus(quoteAmount)
+    openOrder.claimedAmountUSD = openOrder.claimedAmountUSD.plus(amountUSD)
+
+    // claimable data
+    openOrder.claimableUnitAmount = openOrder.claimableUnitAmount.minus(
+      event.params.unit,
+    )
+    openOrder.claimableBaseAmount =
+      openOrder.claimableBaseAmount.minus(baseAmount)
+    openOrder.claimableQuoteAmount =
+      openOrder.claimableQuoteAmount.minus(quoteAmount)
+    openOrder.claimableAmountUSD = openOrder.claimableAmountUSD.minus(amountUSD)
+
+    if (getPendingUnitAmount(openOrder).isZero()) {
+      store.remove('OpenOrder', openOrderID.toString())
+    } else {
+      openOrder.save()
+    }
+  }
+}
--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -3,5 +3,13 @@ import { handleMake } from './book-manager/make'
 import { handleTake } from './book-manager/take'
 import { handleTransfer } from './book-manager/transfer'
 import { handleCancel } from './book-manager/cancel'
+import { handleClaim } from './book-manager/claim'
 
-export { handleOpen, handleMake, handleTake, handleTransfer, handleCancel }
+export {
+  handleOpen,
+  handleMake,
+  handleTake,
+  handleTransfer,
+  handleCancel,
+  handleClaim,
+}
