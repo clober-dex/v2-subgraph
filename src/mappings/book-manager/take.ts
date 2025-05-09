cd29f9c8906f228e05867e0599f79b16e5c78403
graykode <nlkey2022@gmail.com>
2025-05-09 11:28:55 +0900

    refactor: add handleTake function and update exports in core.ts

---
+++ src/mappings/book-manager/take.ts
@@ -0,0 +1,266 @@
+import { BigDecimal, BigInt, log } from '@graphprotocol/graph-ts'
+
+import { Take } from '../../../generated/BookManager/BookManager'
+import { unitToBase, unitToQuote } from '../../common/amount'
+import { OpenOrder, Token } from '../../../generated/schema'
+import { encodeOrderId } from '../../common/order'
+import { ONE_BI, ZERO_BD, ZERO_BI } from '../../common/constants'
+import {
+  getBookOrLog,
+  getDepthOrLog,
+  getTokenOrLog,
+} from '../../common/entity-getters'
+import { convertTokenToDecimal } from '../../common/utils'
+import { calculateValueUSD, getTokenUSDPrice } from '../../common/pricing'
+import {
+  formatInvertedPrice,
+  formatPrice,
+  tickToPrice,
+} from '../../common/tick'
+import { updateBookDayData, updateTokenDayData } from '../interval-updates'
+
+function fillOpenOrder(
+  openOrder: OpenOrder,
+  unitSize: BigInt,
+  quote: Token,
+  quoteInUSD: BigDecimal,
+  base: Token,
+  baseInUSD: BigDecimal,
+  filledUnitAmount: BigInt,
+): void {
+  const updatedFilledUnitAmount =
+    openOrder.filledUnitAmount.plus(filledUnitAmount)
+  openOrder.filledUnitAmount = updatedFilledUnitAmount
+  openOrder.filledBaseAmount = unitToBase(
+    unitSize,
+    updatedFilledUnitAmount,
+    openOrder.priceRaw,
+  )
+  openOrder.filledQuoteAmount = unitToQuote(unitSize, updatedFilledUnitAmount)
+
+  openOrder.filledAmountUSD = calculateValueUSD(
+    convertTokenToDecimal(openOrder.filledQuoteAmount, quote.decimals),
+    quoteInUSD,
+    convertTokenToDecimal(openOrder.filledBaseAmount, base.decimals),
+    baseInUSD,
+  )
+
+  const claimableUnitAfterFill =
+    openOrder.claimableUnitAmount.plus(filledUnitAmount)
+  openOrder.claimableUnitAmount = claimableUnitAfterFill
+  openOrder.claimableBaseAmount = unitToBase(
+    unitSize,
+    claimableUnitAfterFill,
+    openOrder.priceRaw,
+  )
+  openOrder.claimableQuoteAmount = unitToQuote(unitSize, claimableUnitAfterFill)
+  openOrder.claimableAmountUSD = calculateValueUSD(
+    convertTokenToDecimal(openOrder.claimableQuoteAmount, quote.decimals),
+    quoteInUSD,
+    convertTokenToDecimal(openOrder.claimableBaseAmount, base.decimals),
+    baseInUSD,
+  )
+
+  const remainingOpenUnitAmount =
+    openOrder.openUnitAmount.minus(filledUnitAmount)
+  openOrder.openUnitAmount = remainingOpenUnitAmount
+  openOrder.openBaseAmount = unitToBase(
+    unitSize,
+    remainingOpenUnitAmount,
+    openOrder.priceRaw,
+  )
+  openOrder.openQuoteAmount = unitToQuote(unitSize, remainingOpenUnitAmount)
+  openOrder.openAmountUSD = calculateValueUSD(
+    convertTokenToDecimal(openOrder.openQuoteAmount, quote.decimals),
+    quoteInUSD,
+    convertTokenToDecimal(openOrder.openBaseAmount, base.decimals),
+    baseInUSD,
+  )
+
+  if (remainingOpenUnitAmount.lt(ZERO_BI)) {
+    log.error('[TAKE] Negative open unit amount: {}', [openOrder.id])
+  }
+
+  openOrder.save()
+}
+
+export function handleTake(event: Take): void {
+  if (event.params.unit.isZero()) {
+    return
+  }
+  const priceRaw = tickToPrice(event.params.tick)
+  const book = getBookOrLog(event.params.bookId.toString(), 'TAKE')
+  if (book === null) {
+    return
+  }
+
+  const depth = getDepthOrLog(
+    event.params.bookId
+      .toString()
+      .concat('-')
+      .concat(event.params.tick.toString()),
+    'TAKE',
+  )
+  if (depth === null) {
+    return
+  }
+
+  const quote = getTokenOrLog(book.quote, 'TAKE')
+  const base = getTokenOrLog(book.base, 'TAKE')
+  if (quote === null || base === null) {
+    return
+  }
+
+  const takenUnitAmount = event.params.unit
+  const takenBaseAmount = unitToBase(book.unitSize, takenUnitAmount, priceRaw)
+  const takenBaseAmountDecimal = convertTokenToDecimal(
+    takenBaseAmount,
+    base.decimals,
+  )
+  const protocolFeesBase = !book.isTakerFeeInQuote
+    ? takenBaseAmountDecimal.times(book.takerFee)
+    : ZERO_BD
+
+  const takenQuoteAmount = unitToQuote(book.unitSize, takenUnitAmount)
+  const takenQuoteAmountDecimal = convertTokenToDecimal(
+    takenQuoteAmount,
+    quote.decimals,
+  )
+  const protocolFeesQuote = book.isTakerFeeInQuote
+    ? takenQuoteAmountDecimal.times(book.takerFee)
+    : ZERO_BD
+
+  // book data
+  book.price = formatPrice(priceRaw, base.decimals, quote.decimals) // this should be first
+
+  const quoteInUSD = getTokenUSDPrice(quote)
+  const baseInUSD = getTokenUSDPrice(base)
+  const amountTotalUSD = calculateValueUSD(
+    takenQuoteAmountDecimal,
+    quoteInUSD,
+    takenBaseAmountDecimal,
+    baseInUSD,
+  )
+  const protocolFeesTotalUSD = calculateValueUSD(
+    protocolFeesQuote,
+    quoteInUSD,
+    protocolFeesBase,
+    baseInUSD,
+  )
+
+  book.priceRaw = priceRaw
+  book.inversePrice = formatInvertedPrice(
+    priceRaw,
+    base.decimals,
+    quote.decimals,
+  )
+  book.tick = event.params.tick
+  book.volumeQuote = book.volumeQuote.plus(takenQuoteAmountDecimal)
+  book.volumeBase = book.volumeBase.plus(takenBaseAmountDecimal)
+  book.volumeUSD = book.volumeUSD.plus(amountTotalUSD)
+  book.txCount = book.txCount.plus(ONE_BI)
+  book.protocolFeesQuote = book.protocolFeesQuote.plus(protocolFeesQuote)
+  book.protocolFeesBase = book.protocolFeesBase.plus(protocolFeesBase)
+  book.protocolFeesUSD = book.protocolFeesUSD.plus(protocolFeesTotalUSD)
+  book.totalValueLocked = book.totalValueLocked.minus(takenQuoteAmountDecimal)
+  book.totalValueLockedUSD = book.totalValueLockedUSD.minus(amountTotalUSD)
+
+  // depth data
+  depth.unitAmount = depth.unitAmount.minus(takenUnitAmount)
+  depth.quoteAmount = depth.quoteAmount.minus(takenQuoteAmount)
+  depth.baseAmount = depth.baseAmount.minus(takenBaseAmount)
+
+  // quote token data
+  quote.volume = quote.volume.plus(takenQuoteAmountDecimal)
+  quote.volumeUSD = quote.volumeUSD.plus(amountTotalUSD)
+  quote.protocolFees = quote.protocolFees.plus(protocolFeesQuote)
+  quote.protocolFeesUSD = quote.protocolFeesUSD.plus(protocolFeesTotalUSD)
+  quote.txCount = quote.txCount.plus(ONE_BI)
+  quote.totalValueLocked = quote.totalValueLocked.minus(takenQuoteAmountDecimal)
+  quote.totalValueLockedUSD = quote.totalValueLockedUSD.minus(amountTotalUSD)
+
+  // base token data
+  base.volume = base.volume.plus(takenBaseAmountDecimal)
+  base.volumeUSD = base.volumeUSD.plus(amountTotalUSD)
+  base.protocolFees = base.protocolFees.plus(protocolFeesBase)
+  base.protocolFeesUSD = base.protocolFeesUSD.plus(protocolFeesTotalUSD)
+  base.txCount = base.txCount.plus(ONE_BI)
+  // note: do not update base.totalValueLocked
+
+  // interval data
+  const bookDayData = updateBookDayData(book, event)
+  const quoteDayData = updateTokenDayData(quote, quoteInUSD, event)
+  const baseDayData = updateTokenDayData(base, baseInUSD, event)
+
+  // update volume and protocol fees metrics
+  bookDayData.volumeQuote = bookDayData.volumeQuote.plus(
+    takenQuoteAmountDecimal,
+  )
+  bookDayData.volumeBase = bookDayData.volumeBase.plus(takenBaseAmountDecimal)
+  bookDayData.volumeUSD = bookDayData.volumeUSD.plus(amountTotalUSD)
+  bookDayData.txCount = bookDayData.txCount.plus(ONE_BI)
+  bookDayData.protocolFeesQuote =
+    bookDayData.protocolFeesQuote.plus(protocolFeesQuote)
+  bookDayData.protocolFeesBase =
+    bookDayData.protocolFeesBase.plus(protocolFeesBase)
+  bookDayData.protocolFeesUSD =
+    bookDayData.protocolFeesUSD.plus(protocolFeesTotalUSD)
+
+  quoteDayData.volume = quoteDayData.volume.plus(takenQuoteAmountDecimal)
+  quoteDayData.volumeUSD = quoteDayData.volumeUSD.plus(amountTotalUSD)
+  quoteDayData.protocolFees = quoteDayData.protocolFees.plus(protocolFeesQuote)
+  quoteDayData.protocolFeesUSD =
+    quoteDayData.protocolFeesUSD.plus(protocolFeesTotalUSD)
+
+  baseDayData.volume = baseDayData.volume.plus(takenBaseAmountDecimal)
+  baseDayData.volumeUSD = baseDayData.volumeUSD.plus(amountTotalUSD)
+  baseDayData.protocolFees = baseDayData.protocolFees.plus(protocolFeesBase)
+  baseDayData.protocolFeesUSD =
+    baseDayData.protocolFeesUSD.plus(protocolFeesTotalUSD)
+
+  let currentOrderIndex = depth.latestTakenOrderIndex
+  let remainingTakenUnitAmount = takenUnitAmount
+  while (remainingTakenUnitAmount.gt(ZERO_BI)) {
+    const orderID = encodeOrderId(book.id, event.params.tick, currentOrderIndex)
+    const openOrder = OpenOrder.load(orderID.toString())
+    if (openOrder === null) {
+      currentOrderIndex = currentOrderIndex.plus(ONE_BI)
+      continue
+    }
+
+    const openOrderRemainingUnitAmount = openOrder.unitAmount.minus(
+      openOrder.filledUnitAmount,
+    )
+    let filledUnitAmount = ZERO_BI
+    if (remainingTakenUnitAmount.lt(openOrderRemainingUnitAmount)) {
+      filledUnitAmount = remainingTakenUnitAmount
+    } else {
+      filledUnitAmount = openOrderRemainingUnitAmount
+    }
+
+    remainingTakenUnitAmount = remainingTakenUnitAmount.minus(filledUnitAmount)
+
+    fillOpenOrder(
+      openOrder,
+      book.unitSize,
+      quote,
+      quoteInUSD,
+      base,
+      baseInUSD,
+      filledUnitAmount,
+    )
+
+    if (openOrder.unitAmount.equals(openOrder.filledUnitAmount)) {
+      currentOrderIndex = currentOrderIndex.plus(ONE_BI)
+    }
+  }
+
+  depth.latestTakenOrderIndex = currentOrderIndex
+  depth.save()
+  book.save()
+  quote.save()
+  base.save()
+  bookDayData.save()
+  quoteDayData.save()
+  baseDayData.save()
+}
--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -1,4 +1,5 @@
 import { handleOpen } from './book-manager/open'
 import { handleMake } from './book-manager/make'
+import { handleTake } from './book-manager/take'
 
-export { handleOpen, handleMake }
+export { handleOpen, handleMake, handleTake }
