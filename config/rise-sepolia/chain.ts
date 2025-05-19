cdbeddc9f55d4d6aa047d5a896936406525fb04b
graykode <nlkey2022@gmail.com>
2025-05-19 17:28:30 +0900

    feat: add USDT and USDC to stable coins list

--- config/rise-sepolia/chain.ts
+++ config/rise-sepolia/chain.ts
@@ -20,6 +20,8 @@ export const NATIVE_TOKEN_BOOK_ID: BigInt = BigInt.fromString(
 
 export const STABLE_COINS: string[] = [
   '0xa985e387ddf21b87c1fe8a0025d827674040221e', // Clober USDC
+  '0x40918ba7f132e0acba2ce4de4c4baf9bd2d7d849', // USDT
+  '0x8a93d247134d91e0de6f96547cb0204e5be8e5d8', // USDC
 ]
 
 export const MINIMUM_USD_LOCKED = BigDecimal.fromString('0')
