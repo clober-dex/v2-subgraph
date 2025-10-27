5a6b8bdb5e50a45a40520b521b3a5f90da566504
graykode <nlkey2022@gmail.com>
2025-10-27 18:24:03 +0900

    feat: add MONAD_MAINNET to NETWORK enum in prepare-network.ts

--- script/utils/prepare-network.ts
+++ script/utils/prepare-network.ts
@@ -7,6 +7,7 @@ import { Argv } from './argv'
 
 export enum NETWORK {
   MONAD_TESTNET = 'monad-testnet',
+  MONAD_MAINNET = 'monad-mainnet',
   RISE_SEPOLIA = 'rise-sepolia',
   ARBITRUM_SEPOLIA = 'arbitrum-sepolia',
   BERACHAIN_MAINNET = 'berachain-mainnet',
