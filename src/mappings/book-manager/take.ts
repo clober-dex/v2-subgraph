6a0f05d3322c1ca9abb41fe756a281f72ef58e68
graykode <nlkey2022@gmail.com>
2026-01-06 14:36:52 +0900

    fix: add volumeUSD calculation and initialization in handleTake function

--- src/mappings/book-manager/take.ts
+++ src/mappings/book-manager/take.ts
@@ -5,19 +5,22 @@ import {
   CloberDayData,
   ContractInteractionDayData,
 } from '../../../generated/schema'
-import { ONE_BI, ZERO_BI } from '../../common/constants'
+import { ONE_BI, ZERO_BD, ZERO_BI } from '../../common/constants'
+import { tickToPrice } from '../../common/tick'
+import { unitToBase } from '../../common/amount'
+import { convertTokenToDecimal } from '../../common/utils'
 
 export function handleTake(event: Take): void {
   const bookId = event.params.bookId
   if (
     !bookId.equals(
       BigInt.fromString(
-        '3875727077379471850923186002296331935053867847116966170720',
+        '3875727077379471850923186002296331935053867847116966170720', // ask
       ),
     ) &&
     !bookId.equals(
       BigInt.fromString(
-        '5954885684956363054050231031211743946744177791604395877538',
+        '5954885684956363054050231031211743946744177791604395877538', // bid
       ),
     )
   ) {
@@ -26,6 +29,20 @@ export function handleTake(event: Take): void {
   if (!event.transaction.to) {
     return
   }
+  const isTakingBidBook = bookId.equals(
+    BigInt.fromString(
+      '5954885684956363054050231031211743946744177791604395877538',
+    ),
+  )
+  const priceRaw = tickToPrice(event.params.tick)
+  const volumeUsd = isTakingBidBook
+    ? event.params.unit
+    : unitToBase(
+        BigInt.fromString('1000000000000'),
+        event.params.unit,
+        priceRaw,
+      )
+  const volumeUsdBD = convertTokenToDecimal(volumeUsd, BigInt.fromI32(6))
 
   const timestamp = event.block.timestamp.toI32()
   const dayID = timestamp / 86400 // rounded
@@ -51,10 +68,13 @@ export function handleTake(event: Take): void {
     contractInteractionDayData.date = dayStartTimestamp
     contractInteractionDayData.contract = contract
     contractInteractionDayData.callCount = ZERO_BI
+    contractInteractionDayData.volumeUSD = ZERO_BD
   }
 
   contractInteractionDayData.callCount =
     contractInteractionDayData.callCount.plus(ONE_BI)
+  contractInteractionDayData.volumeUSD =
+    contractInteractionDayData.volumeUSD.plus(volumeUsdBD)
   contractInteractionDayData.save()
   cloberDayData.save()
 }
